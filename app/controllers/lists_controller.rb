8470d78b780ae22e0d01ea43198c72c4cf2d3102
Andrew M Westling <a@andrewwestling.com>
2014-09-05 16:07:45 -0700

    add lists#create functionality and update index view to support this

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,6 +2,12 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(params[:list])
+    redirect_to '/'
   end
 
 end
