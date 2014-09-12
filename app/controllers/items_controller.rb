dd295179d32375f047ea0bc5148f65b659dc4269
Andrew M Westling <a@andrewwestling.com>
2014-09-12 10:24:47 -0700

    add item#create method for adding items to lists

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
 
+  def create
+    @list = List.find(params[:id])
+    @item = @list.items.create(params[:item])
+    redirect_to("/lists/#{@list.id}")
+  end
 
 end
